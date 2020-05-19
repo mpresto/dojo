c30bae787476b2cda40e9a7de11a51f30cf7b8cc
Monty Preston <monty.preston5@gmail.com>
2020-05-18 21:58:06 -0700

    Created basic views and routes.

--- python_stack/_django/django_intro/my_proj/my_app/urls.py
+++ python_stack/_django/django_intro/my_proj/my_app/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('new', views.new),
+    path('create', views.create),
+    path('<int:number>', views.show),
+    path('<int:number>/edit', views.edit),
+    path('<int:number>/delete', views.destroy),
 ]
--- python_stack/_django/django_intro/my_proj/my_app/views.py
+++ python_stack/_django/django_intro/my_proj/my_app/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is my response!")
+    return HttpResponse("Placeholder to later display a list of all blogs")
+
+
+def new(request):
+    return HttpResponse("Placeholder to display a new form to create a new blog")
+
+
+def create(request):
+    return HttpResponse("Placeholder for the create blog page")
+
+
+def show(request, number):
+    return HttpResponse(f"Placeholder to display blog number {number}")
+
+
+def edit(request, number):
+    return HttpResponse(f"Placeholder to edit blog {number}")
+
+
+def destroy(request, number):
+    return HttpResponse(f"Here's where we'd delete blog {number}")
