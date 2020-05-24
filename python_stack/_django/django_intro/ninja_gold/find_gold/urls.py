0bba8fbcc5746e79a5d9898e9ae484715098b7e2
Monty Preston <monty.preston5@gmail.com>
2020-05-24 13:50:51 -0700

    Added mining functionality; activities log; ability to bet against the game on number of moves to reach specified gold count.

--- python_stack/_django/django_intro/ninja_gold/find_gold/urls.py
+++ python_stack/_django/django_intro/ninja_gold/find_gold/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('/process_money', views.process)
+    path('process_money', views.process),
+    path('take_bet', views.take_bet),
+    path('reset', views.reset),
 ]
--- python_stack/_django/django_intro/ninja_gold/find_gold/views.py
+++ python_stack/_django/django_intro/ninja_gold/find_gold/views.py
@@ -1,10 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+import random
+import datetime
+
 
 # Create your views here.
-
 def index(request):
-    pass
+    # check for gold in session:
+    if 'gold' in request.session:
+        return render(request, 'find_gold/index.html')
+    # if none, set to 0 and create empty logs
+    else:
+        request.session['move_count'] = 0
+        request.session['gold'] = 0
+        request.session['logs'] = []
+        return render(request, 'find_gold/index.html')
+
+
+def take_bet(request):
+    if 'guess' in request.session:
+        return redirect("/")
+    else:
+        request.session['guess'] = int(request.POST['guess'])
+        return redirect("/")
 
 
 def process(request):
-    pass
+    # make sure the user submitted a bet
+    if 'guess' not in request.session:
+        request.session['message'] = ("You must place a bet!")
+        return redirect("/")
+
+    # get place value from POST and adjust 'gold' accordingly:
+    if request.POST['mine'] == 'casino':
+        earned_gold = random.randint(-50, 50)
+
+    elif request.POST['mine'] == 'farm':
+        earned_gold = random.randint(10, 20)
+
+    elif request.POST['mine'] == 'cave':
+        earned_gold = random.randint(5, 10)
+
+    elif request.POST['mine'] == 'house':
+        earned_gold = random.randint(2, 5)
+
+    # adjust gold 
+    request.session['gold'] += earned_gold
+    # log the activity: 
+    timestamp = datetime.datetime.now().strftime("%d %b %Y %H:%M:%S +0000")
+    if earned_gold > 0:
+        log = (f"Earned {earned_gold} gold from the {request.POST['mine']}! {timestamp}")
+        request.session['logs'].append(log)
+    else:
+        log = (f"\nEntered a casino and lost! {earned_gold} gold. Ouch! {timestamp}")
+        request.session['logs'].append(log)       
+    
+    # increment the move count:
+    request.session['move_count'] += 1
+    # check to see if user won:
+    if request.session['move_count'] < request.session['guess']:
+        moves_left = (request.session['guess'] - request.session['move_count'])
+        request.session['message'] = (f'{moves_left} moves remaining!')
+
+    if request.session['gold'] < 200:
+        if request.session['move_count'] == request.session['guess']:
+            request.session['message'] = ("You lost! Try again.")
+
+    if request.session['gold'] >= 200:
+        if request.session['move_count'] == request.session['guess']:
+            request.session['message'] = ("You won!")
+        else:
+            request.session['message'] = ("Too early! Please try again.")      
+
+    return redirect("/")
+
+
+def reset(request):
+    request.session.flush()
+    return redirect("/")
