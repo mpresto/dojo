3ef66eb217a22ebcb64b33ae15260d09e6ae275c
Monty Preston <monty.preston5@gmail.com>
2020-05-24 20:47:53 -0700

    Refactored urls.py and index.html to include Named Routes.

--- python_stack/_django/django_intro/ninja_gold/find_gold/urls.py
+++ python_stack/_django/django_intro/ninja_gold/find_gold/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('process_money', views.process),
-    path('take_bet', views.take_bet),
-    path('reset', views.reset),
+    path('', views.index, name='home'),
+    path('process_money', views.process, name='process'),
+    path('take_bet', views.take_bet, name='bet'),
+    path('reset', views.reset, name='reset'),
 ]
