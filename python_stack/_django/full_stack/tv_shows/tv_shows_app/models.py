8cf69bfa7101f23874e2b52e54283740375e812c
Monty Preston <monty.preston5@gmail.com>
2020-06-06 09:06:46 -0700

    Added validations for show titles to prevent duplicate entries.

--- python_stack/_django/full_stack/tv_shows/tv_shows_app/models.py
+++ python_stack/_django/full_stack/tv_shows/tv_shows_app/models.py
@@ -9,16 +9,28 @@ class ShowManager(models.Manager):
         errors = {}
         if len(postData['title']) < 2:
             errors['title'] = "Title must be greater than 2 characters."
+
+        if postData['form_type'] != 'create':
+            update_show = Show.objects.get(id=postData['id'])
+            if Show.objects.filter(title__iexact=postData['title']).exists() and postData['title'] != update_show.title:
+                    errors['title'] = "This title already exists."
+        if postData['form_type'] == 'create':
+            if Show.objects.filter(title__iexact=postData['title']).exists():
+                errors['title'] = "This title already exists."
+
         if len(postData['network']) < 3:
             errors['network'] = "Network should be at least 3 characters."
         if len(postData['desc']) != 0 and len(postData['desc']) < 10:
             errors['desc'] = "Description should be at least 10 characters."
+
         if postData['release_date'] == '':
             errors['no_date'] = "Please enter a valid date."
         if postData['release_date'] > date.today().strftime('%Y-%m-%d'):
             errors['future_date'] = "Release date must be in the past."
         print(postData['release_date'])
+
         return errors
+
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
