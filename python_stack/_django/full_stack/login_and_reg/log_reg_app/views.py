85a16386784d7729943da49920bd2257d8862c10
Monty Preston <monty.preston5@gmail.com>
2020-06-09 09:04:48 -0700

    Added bcrypt to registration and updated success.html with session data.

--- python_stack/_django/full_stack/login_and_reg/log_reg_app/views.py
+++ python_stack/_django/full_stack/login_and_reg/log_reg_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from .models import User
+import bcrypt
 
 # Create your views here.
 
@@ -11,7 +12,7 @@ def index(request):
 
 
 def login(request):
-    # validate login form    
+    # validate login
     # include hashing
     # redirect to success page
     return redirect('/success')
@@ -24,29 +25,30 @@ def register(request):
         for key, value in errors.items():
             messages.error(request, value)
         return redirect('/')
-    else:
-        # if no errors, create new user
+    else:  # if no errors, create new user
+        # hash password:
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        print(pw_hash)
+
         new_user = User.objects.create(
             first_name=request.POST['fname'],
             last_name=request.POST['lname'],
             birthday=request.POST['bday'],
             email=request.POST['email'],
-            password=request.POST['password']
+            password=pw_hash
         )
         messages.success(request, "Registration successful!")
         print("Created a new user")
         print(request.POST['bday'])
+        request.session['username'] = new_user.first_name
         # redirect to a success route
         return redirect('/success')
 
 
-
-    
-    # redirect to success page
-    return redirect('/success')
-
-
 def success(request):
     # render success page
-    return render(request, 'success.html')
+    context = {
+    }
+    return render(request, 'success.html', context)
 
