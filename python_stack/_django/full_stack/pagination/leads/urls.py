56e138a3c72712ec70874304df94f5c67aa05faf
Monty Preston <monty.preston5@gmail.com>
2020-06-30 23:48:23 -0700

    WIP paginating with ajax

--- python_stack/_django/full_stack/pagination/leads/urls.py
+++ python_stack/_django/full_stack/pagination/leads/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('?page=<int:id>', views.paginate),
 ]
--- python_stack/_django/full_stack/pagination/leads/views.py
+++ python_stack/_django/full_stack/pagination/leads/views.py
@@ -6,17 +6,23 @@ from .models import Lead
 # Create your views here.
 
 
-# def index(request):
-#     context = {
-#         'all_leads': Lead.objects.all()
-#     }
-#     return render(request, 'index.html', context)
-
-
 def index(request):
+    all_leads = Lead.objects.all()
+    paginator = Paginator(all_leads, 5)  # Show 5 leads per page.
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+    
+    context = {
+            'all_leads': Lead.objects.all(),
+            'page_obj': page_object,
+        }
+    return render(request, 'index.html', context)
+
+
+def paginate(request, id):
     # paginate query results
     all_leads = Lead.objects.all()
-    paginator = Paginator(all_leads, 3)  # Show 3 leads per page.
+    paginator = Paginator(all_leads, 5)  # Show 5 leads per page.
     page_number = request.GET.get('page')
     page_object = paginator.get_page(page_number)
     
@@ -25,4 +31,4 @@ def index(request):
             'page_obj': page_object,
         }
 
-    return render(request, 'index.html', context)
+    return render(request, 'query_results.html', context)
