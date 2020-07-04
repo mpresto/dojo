7df91852267fafd6f0d4b81b1fbd871c6c9d5c65
Monty Preston <monty.preston5@gmail.com>
2020-07-03 20:55:06 -0700

    WIP: Adding in search functionality to form with AJAX. Views, routes and templates are pointed; working on script file.

--- python_stack/_django/full_stack/pagination/leads/urls.py
+++ python_stack/_django/full_stack/pagination/leads/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('?page=<int:id>', views.paginate),
+    path('leadsearch', views.search),
 ]
--- python_stack/_django/full_stack/pagination/leads/views.py
+++ python_stack/_django/full_stack/pagination/leads/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from django.core.paginator import Paginator
 from .models import Lead
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 # Create your views here.
@@ -20,7 +23,7 @@ def index(request):
 
 
 def paginate(request, id):
-    # paginate query results
+    # paginate all leads
     all_leads = Lead.objects.all()
     paginator = Paginator(all_leads, 5)  # Show 5 leads per page.
     page_number = request.GET.get('page')
@@ -30,4 +33,26 @@ def paginate(request, id):
             'all_leads': Lead.objects.all(),
             'page_obj': page_object,
         }
-    return render(request, 'query_results.html', context)
+    return render(request, 'snippets/query_results.html', context)
+
+
+@csrf_exempt
+def search(request):
+    # apply filter
+    userinput = request.GET['first']
+    result = Lead.objects.filter(first_name__icontains=userinput)
+    if result:
+        messages.success(request, "Your search returned the following:")
+    else:
+        messages.error(request, "There are no matching leads.")
+
+    # paginate results
+    paginator = Paginator(result, 5)  # Show 5 leads per page.
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+
+    context = {
+            'result': result,
+            'page_obj': page_object,
+        }
+    return render(request, 'snippets/query_results.html', context)
