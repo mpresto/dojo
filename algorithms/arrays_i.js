bfa914c4cb3670abd10b4eb2ea654f14b25e662e
Monty Preston <monty.preston5@gmail.com>
2020-07-10 18:14:10 -0700

    minToFront completed.

--- algorithms/arrays_i.js
+++ algorithms/arrays_i.js
@@ -116,3 +116,31 @@ function removeDupes(arr){
 removeDupes(['a', 'a', 'b', 'c', 'c', 'd', 'd', 'e'])
 
 // Second: Solve this without using any nested loops.
+
+
+
+// Min to Front
+// Given an array of comparable values, move the lowest element to array’s front,
+// shifting backward any elements previously ahead of it. Do not otherwise change 
+// the array’s order. Given [4,2,1,3,5], change it to [1,4,2,3,5] and return it. 
+// As always, do this without using built-in functions.
+
+function minToFront(arr){
+    var min = arr[0];
+    var minIdx = 0;
+    for (var i=1; i<arr.length; i++){
+        if (arr[i] < min){
+            min = arr[i];
+            minIdx = i;
+        }
+    }
+    for (var x = minIdx; x > 0; x--){
+        var temp = arr[x];
+        arr[x] = arr[x-1];
+        arr[x-1] = temp;
+    }
+    console.log(arr);
+    return arr;
+}
+
+minToFront([4,2,1,3,5])
