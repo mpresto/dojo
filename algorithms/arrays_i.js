8df556108d542d436caf55a89d17aca2774fc6d5
Monty Preston <monty.preston5@gmail.com>
2020-07-12 14:21:45 -0700

    Completed reverse, rotation with iterations to account for edge-cases, filterig and concatenation of arrays.

--- algorithms/arrays_i.js
+++ algorithms/arrays_i.js
@@ -1,3 +1,4 @@
+// ToDo1
 // Push Front
 // Given an array and an additional value, insert this value at the beginning of the array. 
 //Do this without using any built-in array methods.
@@ -118,6 +119,8 @@ removeDupes(['a', 'a', 'b', 'c', 'c', 'd', 'd', 'e'])
 // Second: Solve this without using any nested loops.
 
 
+//########################
+
 
 // Min to Front
 // Given an array of comparable values, move the lowest element to array’s front,
@@ -144,3 +147,161 @@ function minToFront(arr){
 }
 
 minToFront([4,2,1,3,5])
+
+
+//########################
+
+
+// To Do 2
+// Reverse
+// Given a numerical array, reverse the order of values, in-place. The reversed array should have 
+// the same length, with existing elements moved to other indices so that order of elements is reversed. 
+// Working ‘in-place’ means that you cannot use a second array – move values within the array that you 
+// are given. As always, do not use built-in array functions such as splice().
+
+function reverseArray(arr){
+    for (var i = 0; i < (arr.length-1)/2; i++){
+        var temp = arr[i];
+        arr[i] = arr[arr.length-1-i];
+        arr[arr.length-1-i] = temp;
+    }
+    console.log(arr);
+}
+reverseArray([4,2,1,3,5])
+
+
+// Rotate
+// Implement rotateArr(arr, shiftBy) that accepts array and offset. Shift arr’s 
+// values to the right by that amount. ‘Wrap-around’ any values that shift off 
+// array’s end to the other side, so that no data is lost. Operate in-place: 
+// given ([1,2,3],1), change the array to [3,1,2]. Don’t use built-in functions.
+
+function rotateArr(arr, shiftBy){
+    while (shiftBy > 0){
+        var temp = arr[arr.length-1]
+        for (var i = arr.length-1; i>=0; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;  
+        shiftBy--;     
+    }
+    console.log(arr)
+}
+
+rotateArr([1,2,3],1)
+
+// Second: allow negative shiftBy (shift L, not R).
+
+function rotateArr(arr, shiftBy){
+    if (shiftBy > 0){
+        while (shiftBy > 0){
+            var temp = arr[arr.length-1]
+            for (var i = arr.length-1; i>=0; i--){
+                arr[i] = arr[i-1];
+            }
+            arr[0] = temp;  
+            shiftBy--;     
+        }
+        console.log(arr)
+        return arr;
+    }
+    else {
+        while (shiftBy < 0){
+            var temp = arr[0]
+            for (var i = 0; i<arr.length; i++){
+                arr[i] = arr[i+1];
+            }
+            arr[arr.length-1] = temp;  
+            shiftBy++;     
+        }
+        console.log(arr)
+        return arr;
+    }
+}
+
+rotateArr([1,2,3],-1)
+
+
+// Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
+
+// Fourth: minimize the touches of each element.
+
+function rotateArr(arr, shiftBy){
+    if (arr.length-1 % shiftBy ==0){
+        return arr;
+    }
+    else if (shiftBy > 0){
+        while (shiftBy > 0){
+            var temp = arr[arr.length-1]
+            for (var i = arr.length-1; i>=0; i--){
+                arr[i] = arr[i-1];
+            }
+            arr[0] = temp;  
+            shiftBy--;     
+        }
+        console.log(arr)
+        return arr;
+    }
+    else {
+        while (shiftBy < 0){
+            var temp = arr[0]
+            for (var i = 0; i<arr.length; i++){
+                arr[i] = arr[i+1];
+            }
+            arr[arr.length-1] = temp;  
+            shiftBy++;     
+        }
+        console.log(arr)
+        return arr;
+    }
+}
+
+rotateArr([1,2,3,4,5,6],2)
+
+
+// Filter Range
+// Alan is good at breaking secret codes. One method is to eliminate values that 
+// lie within a specific known range. Given arr and values min and max, retain 
+// only the array values between min and max. Work in-place: return the array 
+// you are given, with values in original order. No built-in array functions.
+
+
+function filterRange(arr, min, max) {
+	for(var i = 0; i < arr.length; i++) {
+		if(arr[i] > min && arr[i] < max) {
+			for(var j = i; j < arr.length - 1; j++) {
+                console.log(arr)
+                arr[j] = arr[j + 1];
+            }
+            console.log(arr);
+            arr.length = arr.length - 1;
+            console.log(arr);
+            i--;
+            console.log(i);
+		}
+    }
+    console.log(arr);
+    return arr;
+}
+filterRange([3,7,-5,11,4,19,2,8],0,7)
+
+// Concat
+// Replicate JavaScript’s concat(). Create a standalone function that accepts 
+// two arrays. Return a new array containing the first array’s elements,
+// followed by the second array’s elements. Do not alter the original arrays. 
+// Ex.: arrConcat( ['a','b'], [1,2] ) should return new array ['a','b',1,2].
+
+function arrConcat(arr1, arr2){
+    var newArr = [];
+    for (var i = 0; i < arr1.length; i++){
+        newArr.push(arr1[i]);
+    }
+    for (var j = 0; j < arr2.length; j++){
+        newArr.push(arr2[j]);
+    }
+    console.log(arr1);
+    console.log(arr2);
+    console.log(newArr);
+}
+
+arrConcat(['a','b'],[1,2])
