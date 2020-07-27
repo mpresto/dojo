afe22f56ca5c654ed5b6c29e9cd37eb33af8c0ae
Monty Preston <monty.preston5@gmail.com>
2020-07-26 21:15:30 -0700

    Added back(), removeBack() and addBack() functions to SLL class, returning last node value, removing last node, and adding last node when given a value, respectively.

--- algorithms/linked_lists.js
+++ algorithms/linked_lists.js
@@ -72,14 +72,6 @@ class SLL{
     }
 
 
-
-//////////////
-
-    ListNode(value) {
-        this.val = value;
-        this.next = null;
-    }
-
 //Length
 // July 20, 2013: about 5000 people wait in line for a chance to audition 
 // for American Idol. Create a function that accepts a pointer to the first 
@@ -162,6 +154,48 @@ class SLL{
         return avg;
     }
 
+
+// Back/Remove/Add
+// SList: Back
+// Create a function that accepts a ListNode pointer and returns the last value in the list.
+
+    back(){
+        var runner = this.head;
+        while (runner.next) {
+            runner = runner.next;
+        }
+        console.log(runner.value);
+        return runner.value;
+    }
+
+
+// SList: Remove Back
+// Create a standalone function that removes the last ListNode in the list and returns the new list.
+
+    removeBack(){
+        var runner = this.head;
+        while(runner.next.next) {
+            runner = runner.next;
+        }
+        runner.next = null;
+        this.display();
+        return this;
+    }
+
+// SList: Add Back
+// Create a function that creates a ListNode with given value and inserts it at end of a linked list.    
+
+    addBack(value){
+        var runner = this.head;
+        while (runner.next) {
+            runner = runner.next;
+        }
+        runner.next = new Node(value);
+        this.display();
+        return this;
+    }
+
+
 }   
 
 var my_list = new SLL();
@@ -173,3 +207,5 @@ my_list.display()
 my_list.max()
 my_list.min()
 my_list.average()
+my_list.back()
+my_list.removeBack().addBack(18)
