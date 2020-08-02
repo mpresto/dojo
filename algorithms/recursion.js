986d1bdfc11c03c08de3fd6ae381bad217fd8e57
Monty Preston <monty.preston5@gmail.com>
2020-08-02 17:20:09 -0400

    Recursive sigma and factorial exercises.

---
+++ algorithms/recursion.js
@@ -0,0 +1,41 @@
+// To Do 1
+
+// Recursive Sigma
+
+// Write a recursive function that given a number returns the sum of integers 
+// from 1 to that number.
+// Example: rSigma(5) = 15 (1+2+3+4+5); 
+// rSigma(2.5) = 3 (1+2); rSigma(-1) = 0.
+
+
+function rSigma(num){
+    if (num <= 0 ){
+        return 0;
+    }
+    if (num == 1){
+        return num;
+    } else{
+        return num + rSigma(num-1);
+    }
+  }
+
+console.log(rSigma(5))
+
+// Recursive Factorial
+// Given num, return the product of ints from 1 up to num. If less than zero, 
+// treat as zero. If not an integer, truncate. 
+// Experts tell us 0! is 1. rFact(3) = 6 (1*2*3). 
+// Also, rFact(6.5) = 720 (1*2*3*4*5*6).
+
+
+function rFact(num){
+    if (num <= 0) {
+        return 1;
+    }
+    else {
+        return Math.floor(num) * rFact(Math.floor(num) - 1);
+    }
+}
+
+console.log(rFact(3))
+console.log(rFact(6.5))
