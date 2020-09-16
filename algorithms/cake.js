05cc953519000e0e16f71032f86b6f26dc9d3998
Monty Preston <monty.preston5@gmail.com>
2020-09-16 08:56:21 -0700

    Pushing my daily algo practice ------- High Scores using a counting/hashmap strategy.

--- algorithms/cake.js
+++ algorithms/cake.js
@@ -134,3 +134,36 @@ function mergeArrays(myArray, alicesArray) {
     return orders;
   }
   
+
+
+  // HIGHEST SCORE
+
+  function sortScores(unorderedScores, highestPossibleScore) {
+  
+    // if no unorderedScores, return [];
+  
+    // Array of 0s at indices 0..highestPossibleScore
+    const scoreCounts = new Array(highestPossibleScore + 1).fill(0);
+    // 00000000000000000000000000...
+  
+    //Populated scoreCounts with count of scores
+    for (let s = 0; s < unorderedScores.length; s++){
+      scoreCounts[unorderedScores[s]]++;
+    }
+    
+    // console.log(scoreCounts);
+    
+    // create array to hold ordered values
+    ordered = [];
+    
+    // iterate through scoreCounts and push index to new array per the count
+    for (let c = highestPossibleScore; c >= 0; c--){
+      const count = scoreCounts[c];
+      
+        for (time = count; time > 0 ; time--){
+          ordered.push(c);
+        }
+    }
+     return ordered;
+  }
+  
